553e83df78c6576472be5bc126a2ee6d174e8b8e
Giuseppe Iannelli <giuseppe.iannelli@mosaicoon.com>
2017-10-18 15:37:52 +0200

    fix

--- docker/scripts/docker-entrypoint.sh
+++ docker/scripts/docker-entrypoint.sh
@@ -1,29 +1,27 @@
 #/bin/sh
 
 set -e
-
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
+if [ "${1#-}" != "$1" ]
+then
+  echo "[INFO] Set value into /kafka-manager-${KM_VERSION}/conf/application.conf"
 
   echo "kafka-manager.offset-cache-thread-pool-size=$(nproc)"  >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.kafka-admin-client-thread-pool-size$(nproc)"  >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.broker-view-thread-pool-size=3" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.broker-view-max-queue-size=50" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.broker-view-update-seconds=3" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.kafka-admin-client-thread-pool-size=$(nproc)" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.broker-view-thread-pool-size=$(( $((KAFKA_NUMBERS_OF_BROKERS)) * 3))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.broker-view-max-queue-size=$(( $((KAFKA_TOTAL_PARTITIONS_NUMBER)) * $((KAFKA_NUMBERS_OF_BROKERS))))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.broker-view-update-seconds=$(( $((KAFKA_TOTAL_PARTITIONS_NUMBER)) * $((KAFKA_NUMBERS_OF_BROKERS)) / $((10)) * $((KAFKA_NUMBERS_OF_BROKERS)) ))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
 
 
   if [ ! -z $KM_USERNAME ] && [ ! -z $KM_PASSWORD ]; then
-      sed -i.bak '/^basicAuthentication/d' /kafka-manager-${KM_VERSION}/conf/application.conf
-      echo 'basicAuthentication.enabled=true' >> /kafka-manager-${KM_VERSION}/conf/application.conf
-      echo "basicAuthentication.username=${KM_USERNAME}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-      echo "basicAuthentication.password=${KM_PASSWORD}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-      echo 'basicAuthentication.realm="Kafka-Manager"' >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    sed -i.bak '/^basicAuthentication/d' /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo 'basicAuthentication.enabled=true' >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo "basicAuthentication.username=${KM_USERNAME}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo "basicAuthentication.password=${KM_PASSWORD}" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+    echo 'basicAuthentication.realm="Kafka-Manager"' >> /kafka-manager-${KM_VERSION}/conf/application.conf
   fi
 
-
-  /kafka-manager-${KM_VERSION}/bin/kafka-manager -Dconfig.file=/kafka-manager-${KM_VERSION}/application.conf $@
+  echo "[INFO] Start kafka-manager"
+  /kafka-manager-${KM_VERSION}/bin/kafka-manager -Dconfig.file=/kafka-manager-${KM_VERSION}/conf/application.conf $@
 else
-
-	exec "$@"
-
+  $@
 fi
