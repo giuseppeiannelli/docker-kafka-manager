207285aeac07ed87b07ab20c892594f7482d5f95
Giuseppe Iannelli <giuseppe.iannelli@mosaicoon.com>
2017-10-25 11:44:25 +0200

    fix entrypoint

--- docker/scripts/docker-entrypoint.sh
+++ docker/scripts/docker-entrypoint.sh
@@ -5,12 +5,14 @@ if [ "${1#-}" != "$1" ]
 then
   echo "[INFO] Set value into /kafka-manager-${KM_VERSION}/conf/application.conf"
 
-  echo "kafka-manager.offset-cache-thread-pool-size=$(nproc)"  >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.kafka-admin-client-thread-pool-size=$(nproc)" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.broker-view-thread-pool-size=$(( $((KAFKA_NUMBERS_OF_BROKERS)) * 3))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.broker-view-max-queue-size=$(( $((KAFKA_TOTAL_PARTITIONS_NUMBER)) * $((KAFKA_NUMBERS_OF_BROKERS))))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-  echo "kafka-manager.broker-view-update-seconds=$(( $((KAFKA_TOTAL_PARTITIONS_NUMBER)) * $((KAFKA_NUMBERS_OF_BROKERS)) / $((10)) * $((KAFKA_NUMBERS_OF_BROKERS)) ))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
-
+  echo "kafka-manager.offset-cache-thread-pool-size=$((2+nproc))"  >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.offset-cache-thread-pool-size=$((10+nproc))"  >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.kafka-admin-client-thread-pool-size=$((2+nproc))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.logkafka-update-period-seconds=$((10+nproc))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.broker-view-thread-pool-size=$(( ${KAFKA_NUMBER_OF_BROKERS} * 3))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.broker-view-thread-pool-size=$(( ${KAFKA_NUMBER_OF_BROKERS} * 3))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.broker-view-max-queue-size=$(( ${KAFKA_TOTAL_PARTITIONS_NUMBER} * ${KAFKA_NUMBER_OF_BROKERS} ))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
+  echo "kafka-manager.broker-view-update-seconds=$(( ${KAFKA_TOTAL_PARTITIONS_NUMBER} * ${KAFKA_NUMBER_OF_BROKERS} / 10 * ${KAFKA_NUMBER_OF_BROKERS} ))" >> /kafka-manager-${KM_VERSION}/conf/application.conf
 
   if [ ! -z $KM_USERNAME ] && [ ! -z $KM_PASSWORD ]; then
     sed -i.bak '/^basicAuthentication/d' /kafka-manager-${KM_VERSION}/conf/application.conf
